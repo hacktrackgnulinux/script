8e20416377430c3a67e32d3b276b2d84c7d47854
Dindin Hernawan <dindin_hernawan@yahoo.com>
2020-04-21 11:48:18 +0700

    Perbaikan dan Pergantian Based kembali ke awal

--- debootstrap.sh
+++ debootstrap.sh
@@ -4,24 +4,24 @@
 #  / / /  | |_| |/ _` |/ __| |/ / | | || '__/ _` |/ __| |/ /
 #  \ \ \  |  _  | (_| | (__|   <| | | || | | (_| | (__|   < 
 #   \_\_\ |_| |_|\__,_|\___|_|\_\ | |_||_|  \__,_|\___|_|\_\
-#   "my dream is a good hacker" |_|                         
+#   "Good Luck & Think Positive"|_|                         
 # ------------------------------------------------------------
 ###################################################################
-# Default Profile << Hack|Track                        
-# version           : 2019.3
-# Author            : Root HackTrack <root@hacktrack-linux.org>
-# Licenced          : Copyright 2017 GNU GPLv3
-# Website           : http://www.hacktrack-linux.org/
+# Default Profile <<Hack|Track GNU/Linux                       
+# version           : 2020.1
+# Author            : <<Hack|Track GNU/Linux <hacktracklinux@yahoo.com>
+# Licenced          : Copyright 2017-2020 GNU GPLv3
+# Website           : http://www.hacktrack-linux.blogspot.com/
 ###################################################################
 # Script Arsip Debootstrap Hacktrack
 # make folder work
 cd $HOME/$(whoami)/
 mkdir $HOME/$(whoami)/hacktrack 
 cd $HOME/$(whoami)/hacktrack
-mkdir -p image/{live,isolinux,.disk}
-sudo debootstrap --arch=i386 --variant=minbase kali-rolling $HOME/$(whoami)/hacktrack/chroot http://http.kali.org/kali/
-sudo mount --bind /dev/ chroot/dev/
-sudo cp /etc/resolv.conf chroot/etc/
+sudo mkdir -p image/{live,isolinux,.disk}
+sudo debootstrap --arch=i386 --variant=minbase jessie $HOME/$(whoami)/hacktrack/chroot http://ftp.us.debian.org/debian/
+sudo mount --bind /dev/ ./chroot/dev/
+sudo cp /etc/resolv.conf ./chroot/etc/
 sudo chroot chroot
 export HOME=/root
 export LC_ALL=C
@@ -35,11 +35,10 @@ mkdir Desktop Documents Downloads Music Pictures Public Templates Videos
 cd /etc/apt/
 nano sources.list
 # Core Kali-Rolling
-deb http://http.kali.org/kali/ kali-rolling main contrib non-free
+deb http://ftp.us.debian.org/debian/ jessie main contrib non-free
 
 apt-get update
 apt-get install --no-install-recommends linux-image-686 live-boot systemd-sysv network-manager net-tools wireless-tools wpagui xserver-xorg-core xserver-xorg xinit nano
-apt-get install --no-install-recommends kali-linux-forensic binwalk pngcheck hexedit 
 apt-get install mate-core mate-desktop-environment-extra mate-desktop-environment-extras
 
 apt-get clean && apt-get autoremove && rm -rf /tmp/* ~/.bash_history
@@ -69,7 +68,7 @@ cd image/
 sudo rm MD5SUMS
 find -type f -print0 | sudo xargs -0 md5sum | grep -v isolinux/boot.cat | sudo tee MD5SUMS
 cd ..
-sudo mkisofs -r -V "hacktrack-2019.3-i386" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o ../hacktrack-2017.1CORE-i386.iso image
-cd .. && sudo chmod 777 hacktrack-2019.3-i386.iso
-isohybrid hacktrack-2019.3-i386.iso
-md5sum hacktrack-2019.3-i386.iso > hacktrack-2019.3-i386.iso.md5sums
+sudo mkisofs -r -V "hacktrack-2020.1-i386" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o ../hacktrack-2017.1CORE-i386.iso image
+cd .. && sudo chmod 777 hacktrack-2020.1-i386.iso
+isohybrid hacktrack-2020.1-i386.iso
+md5sum hacktrack-2020.1-i386.iso > hacktrack-2020.1-i386.iso.md5sums
